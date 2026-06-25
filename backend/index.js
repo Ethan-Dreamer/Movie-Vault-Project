5eae04691e987654eeadf69c5feb58ac5daa4e90
Rana M. Ahmad <ahmadzafar3546@gmail.com>
2026-06-26 02:59:09 +0500

    Changes to backend

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
     },
   })
 );
