e5a36ed0697f1ed51fe731254716be356e878d6e
Rana M. Ahmad <ahmadzafar3546@gmail.com>
2026-06-26 11:20:08 +0500

    Bug fixes in the backend

--- backend/index.js
+++ backend/index.js
@@ -277,7 +277,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/movievault",
+      callbackURL: `${process.env.VITE_API_URL}/auth/google/movievault`,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
